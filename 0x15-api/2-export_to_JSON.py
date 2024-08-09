9d79e9be26dbb581799794ccfe1d2b1a5465f1d0
LoayAbassi <abassi.loay@gmail.com>
2024-08-09 19:35:50 +0100

    testing

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,19 +11,21 @@ if __name__ == "__main__":
     userID = int(sys.argv[1])
     # retreiving employee's name
     user = requests.get(url+"users/"+str(userID))
-    user = user.json()["name"]
+
+    user = user.json()["username"]
 
     # retreiving employee's todo list
-    
-    tasks = requests.get(url+"todos/")
-    for task in tasks.json():
-        if task["userId"] == int(userID):
-            total += 1
-            if task["completed"]:
-                completed += 1
-                Ncompleted.append(task["title"])
-    out = f"Employee {user} is done with tasks({completed}/{total}):"
-    print(out)
 
-    for title in Ncompleted:
-        print("\t {}".format(title))
+    todo = requests.get(url+"todos/", params={"userId": userID})
+    todo = todo.json()
+    res = {userID: []}
+    for task in todo:
+        item = {}
+        item["task"] = task["title"]
+        item["completed"] = task["completed"]
+        item["username"] = user
+        res[userID].append(item)
+
+    filename = str(userID)+".json"
+    with open(filename, "w") as file:
+        json.dump(res, file)
