5136267b0a3e825ba0835fa285c6d1f0c9798471
LoayAbassi <abassi.loay@gmail.com>
2024-08-09 18:27:48 +0100

    one more

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+returns infos about employee's todo list
+"""
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    import sys
+    url = "https://jsonplaceholder.typicode.com/"
+    userID = int(sys.argv[1])
+    # retreiving employee's name
+    user = requests.get(url+"users/"+str(userID))
+    name = user.json()["username"]
+    # retreiving employee's todo list
+    tasks = requests.get(url+"todos/", params={"userId": userID})
+    tasks = tasks.json()
+    Data = []
+
+    for task in tasks:
+        data = []
+        data.append(str(userID))
+        data.append(name)
+        data.append(str(task["completed"]))
+        data.append(task["title"])
+        Data.append(data)
+
+    filename = str(userID)+".csv"
+    with open(filename, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerows(Data)
