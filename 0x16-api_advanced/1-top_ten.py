1e2590d77b07136b01b5528a306a815df07fa90e
LoayAbassi <abassi.loay@gmail.com>
2024-08-14 08:06:37 +0100

    now ?

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,16 +7,19 @@ def top_ten(subreddit):
     import requests
 
     endpoint = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
     headers = {"User-Agent": "someAgentProb"}
-    params = {"limit": 9}
+    params = {"limit": 10}  # Corrected limit
+
     response = requests.get(endpoint, headers=headers,
                             allow_redirects=False, params=params)
-    if response.status_code == 200:
-        data = response.json()["data"]["children"]
-        for e in data:
-            post = e["data"]["title"]
-            print(post)
 
-    else:
+    try:
+        if response.status_code == 200:
+            data = response.json().get("data", {}).get("children", [])
+            for e in data:
+                post = e.get("data", {}).get("title", "No Title")
+                print(post)
+        else:
+            print(None)
+    except ValueError:
         print(None)
